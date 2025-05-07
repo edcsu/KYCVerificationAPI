90586228ed652650e701d5f59c2f064bfa61da6b
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-05-07 19:52:51 +0300

    refactor: use a database fixture

--- KYCVerificationAPI.IntegrationTests/VerificationRepositoryIntegrationTests.cs
+++ KYCVerificationAPI.IntegrationTests/VerificationRepositoryIntegrationTests.cs
@@ -5,20 +5,15 @@ using Testcontainers.PostgreSql;
 
 namespace KYCVerificationAPI.IntegrationTests;
 
-public class VerificationRepositoryIntegrationTests : IAsyncLifetime
+public class VerificationRepositoryIntegrationTests : IClassFixture<DatabaseFixture>
 {
-    private readonly PostgreSqlContainer _pgContainer;
-    private AppDbContext _dbContext;
+    private readonly DatabaseFixture _fixture;
     private VerificationRepository _verificationRepository;
     
-    public VerificationRepositoryIntegrationTests()
+    public VerificationRepositoryIntegrationTests(DatabaseFixture fixture)
     {
-        _pgContainer = new PostgreSqlBuilder()
-            .WithDatabase("testdb")
-            .WithUsername("postgres")
-            .WithPassword("postgres")
-            .WithCleanUp(true)
-            .Build();
+        _fixture = fixture;
+        _verificationRepository = new VerificationRepository(_fixture.DbContext);
     }
     
     [Fact]
@@ -33,24 +28,5 @@ public class VerificationRepositoryIntegrationTests : IAsyncLifetime
         // Assert
         Assert.NotNull(result);
         Assert.Equal(newVerification, result);
-    }
-
-    public async Task InitializeAsync()
-    {
-        await _pgContainer.StartAsync();
-
-        var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseNpgsql(_pgContainer.GetConnectionString())
-            .Options;
-
-        _dbContext = new AppDbContext(options);
-        await _dbContext.Database.EnsureCreatedAsync();
-
-        _verificationRepository = new VerificationRepository(_dbContext);
-    }
-
-    public async Task DisposeAsync()
-    {
-        await _pgContainer.DisposeAsync();
     }
 }
