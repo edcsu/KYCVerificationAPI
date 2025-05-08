17020764ec468b609eada3b29fdabccfb982594e
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-05-08 14:12:28 +0300

    feat: add filter dto

---
+++ KYCVerificationAPI/Features/Verifications/Requests/VerificationFilter.cs
@@ -0,0 +1,20 @@
+namespace KYCVerificationAPI.Features.Verifications.Requests;
+
+public class VerificationFilter
+{
+    private int _pageSize = 10;
+    private const int MaxPageSize = 50;
+
+    public string? Status { get; set; }
+    public bool? IsMatch { get; set; }
+    public string? Nin { get; set; }
+
+    public int? Page { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+
+}
