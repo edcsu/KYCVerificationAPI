18374bab7d79a200c13f6c88f441561a7da94b40
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-05-27 18:33:35 +0300

    test: add more validation tests

--- KYCVerificationAPI.IntegrationTests/VerificationIntegrationTests.cs
+++ KYCVerificationAPI.IntegrationTests/VerificationIntegrationTests.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using Shouldly;
 using KYCVerificationAPI.Core;
+using KYCVerificationAPI.Features.Verifications.Requests;
 using KYCVerificationAPI.Features.Verifications.Responses;
 using Microsoft.AspNetCore.Mvc;
 
@@ -99,6 +100,118 @@ public class VerificationIntegrationTests : IClassFixture<KycWebApplicationFacto
         var error = validationResult?.Errors.FirstOrDefault();
         error?.Key.ShouldBe("$");
         error?.Value.First().ShouldBe("JSON deserialization for type 'KYCVerificationAPI.Features.Verifications.Requests.CreateVerification' was missing required properties including: 'firstName', 'givenName', 'dateOfBirth', 'nin', 'cardNumber'.");
+    }
+    
+    [Fact]
+    public async Task CreateVerification_ShouldReturnBadRequest_WhenCardNumberIsInvalid()
+    {
+        // Arrange
+        var invalidCardNumber = string.Empty;
+        var invalidRequest = new CreateVerification
+        {
+            FirstName = "Jane",
+            GivenName = "Doe",
+            DateOfBirth = DateOnly.Parse("2012-02-02"),
+            CardNumber = invalidCardNumber,
+            Nin = "12345678901234"
+        };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/verifications", invalidRequest);
+        var validationResult = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        validationResult?.Errors.Count.ShouldBe(1);
+        validationResult?.Title.ShouldBe("One or more validation errors occurred.");
+        
+        var error = validationResult?.Errors.FirstOrDefault();
+        error?.Key.ShouldBe("CardNumber");
+        error?.Value.First().ShouldBe("The field CardNumber must be a string or collection type with a minimum length of '9' and maximum length of '9'.");
+    }
+    
+    [Fact]
+    public async Task CreateVerification_ShouldReturnBadRequest_WhenNinIsInvalid()
+    {
+        // Arrange
+        var invalidNin = "1234";
+        var invalidRequest = new CreateVerification
+        {
+            FirstName = "Jane",
+            GivenName = "Doe",
+            DateOfBirth = DateOnly.Parse("2012-02-02"),
+            CardNumber = "123456789",
+            Nin = invalidNin
+        };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/verifications", invalidRequest);
+        var validationResult = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        validationResult?.Errors.Count.ShouldBe(1);
+        validationResult?.Title.ShouldBe("One or more validation errors occurred.");
+        
+        var error = validationResult?.Errors.FirstOrDefault();
+        error?.Key.ShouldBe("Nin");
+        error?.Value.First().ShouldBe("The field Nin must be a string or collection type with a minimum length of '14' and maximum length of '14'.");
+    }
+    
+    [Fact]
+    public async Task CreateVerification_ShouldReturnBadRequest_WhenFirstNameIsInvalid()
+    {
+        // Arrange
+        var invalidFirstName = "J";
+        var invalidRequest = new CreateVerification
+        {
+            FirstName = invalidFirstName,
+            GivenName = "Doe",
+            DateOfBirth = DateOnly.Parse("2012-02-02"),
+            CardNumber = "123456789",
+            Nin = "12345678901234"
+        };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/verifications", invalidRequest);
+        var validationResult = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        validationResult?.Errors.Count.ShouldBe(1);
+        validationResult?.Title.ShouldBe("One or more validation errors occurred.");
+        
+        var error = validationResult?.Errors.FirstOrDefault();
+        error?.Key.ShouldBe("FirstName");
+        error?.Value.First().ShouldBe("The field FirstName must be a string or collection type with a minimum length of '2' and maximum length of '100'.");
+    }
+    
+    [Fact]
+    public async Task CreateVerification_ShouldReturnBadRequest_WhenGivenNameIsInvalid()
+    {
+        // Arrange
+        var invalidGivenName = "E";
+        var invalidRequest = new CreateVerification
+        {
+            FirstName = "Jane",
+            GivenName = invalidGivenName,
+            DateOfBirth = DateOnly.Parse("2012-02-02"),
+            CardNumber = "123456789",
+            Nin = "12345678901234"
+        };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/verifications", invalidRequest);
+        var validationResult = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        validationResult?.Errors.Count.ShouldBe(1);
+        validationResult?.Title.ShouldBe("One or more validation errors occurred.");
+        
+        var error = validationResult?.Errors.FirstOrDefault();
+        error?.Key.ShouldBe("GivenName");
+        error?.Value.First().ShouldBe("The field GivenName must be a string or collection type with a minimum length of '2' and maximum length of '100'.");
     }
 
     [Fact]
